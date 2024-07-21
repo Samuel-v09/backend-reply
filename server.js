a727a64aa89915b14e7ac955de561144246c89b2
Samuel Victor <samuellwat978@gmail.com>
2024-07-20 23:55:31 -0300

    Teste Render 10

--- server.js
+++ server.js
@@ -2,29 +2,11 @@ const express = require('express');
 const app = express();
 const PORTA = 3000;
 
-require('dotenv').config();
 app.use(express.json());
 
-
-const {Pool} = require ('pg')
-const pool = new Pool({
-    user: process.env.user,
-    host: process.env.host,
-    database: process.env.database,
-    password: process.env.password,
-    port: process.env.port
+app.get('/', (req, res) =>{
+    res.send('Hello World');
 });
-
-pool.query('SELECT NOW()' , (err, res) => {
-    if (err){
-        console.error('Erro ao conectar ao Banco de Dados', err);
-    } else {
-        console.log('Conexão bem-sucedida:', res.rows);
-    }
-
-});
-
-
 
 app.listen(PORTA, () => {
      console.log(`Servidor conectado e rodando em http://localhost:${PORTA}`);
