5914ce2ff7fb2e5b94c4c2bbc0c6366917027059
Samuel Victor <samuellwat978@gmail.com>
2024-07-21 00:45:51 -0300

    Teste Render 11

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+const {Pool} = require('pg');
 const PORTA = 3000;
 
+require('dotenv').config();
 app.use(express.json());
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
 
 app.get('/', (req, res) =>{
     res.send('Hello World');
+});
+
+app.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0]);
 });
 
 app.listen(PORTA, () => {
