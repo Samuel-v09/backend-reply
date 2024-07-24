5ec8d3573ccf65fdb0d0e865aa8467ce2951621f
Samuel Victor <samuellwat978@gmail.com>
2024-07-23 21:10:57 -0300

    Conexão com Banco de Dados e Criação de Tabelas

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const {Pool} = require('pg');
-const PORTA = 3000;
+const PORTA = 3001;
 
 require('dotenv').config();
 app.use(express.json());
@@ -11,24 +11,47 @@ const pool = new Pool({
     ssl: process.env.ssl
 });
 
-pool.query(`
-             CREATE TABLE sensores (
-                 Coleta INTEGER PRIMARY KEY AUTOINCREMENT,
-                 MotorID VARCHAR2(10) NOT NULL,
-                 Temperatura NUMBER (4, 2) NOT NULL,
-                 Frequencia NUMBER (4, 2) NOT NULL,
-                 Corrente NUMBER (4, 2) NOT NULL,
-                 Vibracao VARCHAR2(3) NOT NULL,
-                 PicoMax NUMBER (4, 2) NOT NULL,
-                 timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-             )
-         ` , (err, res) => {
-    if (err){
-        console.error('Erro ao Criar Tabela no Banco de Dados', err);
-    } else {
-        console.log('Tabela Criada com Sucesso no Banco de Dados', res.rows);
+const criando_tabelas_motores = async () => {
+    try {
+        await pool.connect();
+        console.log('Conectando ao Banco de Dados challenge_reply_db');
+
+        await pool.query(
+            `
+             CREATE TABLE IF NOT EXISTS Dados_Motor_01 (
+                 ID SERIAL PRIMARY KEY ,
+                 Motor VARCHAR(20),
+                 Temperatura NUMERIC (10, 2) NOT NULL,
+                 Frequencia NUMERIC (10, 2) NOT NULL,
+                 Corrente NUMERIC (10, 2) NOT NULL,
+                 Vibracao VARCHAR(4) NOT NULL,
+                 PicoMax NUMERIC(10, 2) NOT NULL,
+                 DataHora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+             );
+             
+            
+            CREATE TABLE IF NOT EXISTS Dados_Motor_02 (
+                Motor_ID INTEGER PRIMARY KEY ,
+                Temperatura NUMERIC (4, 2) NOT NULL,
+                Frequencia NUMERIC (4, 2) NOT NULL,
+                Corrente NUMERIC (4, 2) NOT NULL,
+                Vibracao VARCHAR(3) NOT NULL,
+                PicoMax NUMERIC(4, 2) NOT NULL,
+                timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+           ` 
+
+        );
+         console.log('Tabelas motores criadas com sucesso');
+    } catch (err) {
+        console.error('Erro ao criar tabelas motores ou tabelas já existentes :', err);
+    } finally {
+        await pool.end();
+        console.log('Conexão com Banco de Dados challenge_reply_db encerrada');
     }
-});
+};
+
+criando_tabelas_motores();
 
 app.get('/', (req, res) =>{
     res.send('Hello World');
@@ -38,52 +61,6 @@ app.listen(PORTA, () => {
      console.log(`Servidor conectado e rodando em http://localhost:${PORTA}`);
 });
 
-// async function criando_or_verifca_tabela() {
-//     let conexao;
-
-//     try {
-//         conexao = await oracledb.getConnection();
-
-//         const verifica_tabela_existe_SQL = `
-//             SELECT COUNT(*) AS count
-//             FROM user_tables
-//             WHERE table_name = 'Dados_Sensores'
-//         `;
-
-//         const result = await conexao.execute(verifica_tabela_existe_SQL);
-//         const verifica_tabela_existe_node = result.rows[0][0] > 0;
-
-//         if (!verifica_tabela_existe_node){
-//             const criando_tabela_SQL = `
-//             CREATE TABLE Dados_Sensores (
-//                 Coleta INTEGER PRIMARY KEY AUTOINCREMENT,
-//                 MotorID VARCHAR2(10) NOT NULL,
-//                 Temperatura NUMBER (4, 2) NOT NULL,
-//                 Frequencia NUMBER (4, 2) NOT NULL,
-//                 Corrente NUMBER (4, 2) NOT NULL,
-//                 Vibracao VARCHAR2(3) NOT NULL,
-//                 PicoMax NUMBER (4, 2) NOT NULL,
-//                 timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-//             )
-//         `;
-//         await conexao.execute(criando_tabela_SQL);
-//         await conexao.commit();
-//         console.log('Tabela Dados_Sensores foi Criada.');
-//         }else {
-//             console.log('Tabela Dados_Sensors já existente.');
-//         }
-//     } catch (err) {
-//         console.error('Erro ao criar a tabela:', err);
-//     } finally {
-//         if (conexao) {
-//             try {
-//                 await conexao.close();
-//             } catch (err) {
-//                 console.error('Erro ao fechar a conexão:', err);
-//             }
-//         }
-//     }
-// }
 
 
 
