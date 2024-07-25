4f5afe290d0c47dc362b51ffc83a3aa39ec1cd6b
Samuel Victor <samuellwat978@gmail.com>
2024-07-24 22:29:36 -0300

    Código com Post e Get

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const criando_tabelas_motores = async () => {
                  Temperatura NUMERIC (10, 2) NOT NULL,
                  Frequencia NUMERIC (10, 2) NOT NULL,
                  Corrente NUMERIC (10, 2) NOT NULL,
-                 Vibracao VARCHAR(4),
+                 Vibracao VARCHAR(10),
                  PicoMax NUMERIC(10, 2) NOT NULL,
                  DataHora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
              );
@@ -47,7 +47,7 @@ const criando_tabelas_motores = async () => {
         console.error('Erro ao criar tabelas motores ou tabelas já existentes :', err);
     } finally {
         //await pool.end();
-        console.log('Conexão com Banco de Dados challenge_reply_db encerrada');
+        //console.log('Conexão com Banco de Dados challenge_reply_db encerrada'); (Corrigir console log com frase adequada)
     }
 };
 
@@ -62,7 +62,6 @@ app.post('/inserir_dados_motor_01', async (req, res) => {
     const client = await pool.connect();
         
     try {
-        
         console.log('Conectando ao Banco de Dados challenge_reply_db para inserir dados do motor 01');
 
         await client.query(
@@ -81,17 +80,50 @@ app.post('/inserir_dados_motor_01', async (req, res) => {
     }
 });
 
-app.get('/', (req, res) =>{
-    res.send('Hello World');
+app.get('/coletando_dados_motor_01', async (req, res) =>{
+
+    let rows;
+    const client = await pool.connect();
+    try {
+        console.log('Conectando ao Banco de Dados challenge_reply_db para inserir dados do motor 01');
+
+        const consulta = await client.query(`SELECT * FROM Dados_Motor_01`);
+        rows = consulta.rows;
+        console.log('Dados do Motor 01 Coletados com Sucesso');
+        res.status(200).json(rows);
+    } catch (err) {
+        console.error('Erro ao coletar dados do motor 01 no banco de dados challenge_reply_db');
+        res.status(500).send('Erro ao Coletar dados do Motor 01.', err);
+    } finally{
+        client.release();
+        console.log('Conexão com Banco de Dados challenge_reply_db liberada');
+    }
+
 });
 
 app.listen(PORTA, () => {
      console.log(`Servidor conectado e rodando em http://localhost:${PORTA}`);
 });
 
+//Tratamento de Erros e Fechamento do banco de dados
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo deu errado capturado pelo middleware!');
+});
 
+process.on('SIGTERM', () => {
+    pool.end(() => {
+        console.log('Pool de Conexões fechado Termino do processo');
+        process.exit(0);
+    });
+}); 
 
-
+process.on('SIGINT', () => {
+    pool.end(() => {
+        console.log('Pool de Conexões fechado Termino do processo efetuado pelo terminal');
+        process.exit(0);
+    });
+}); 
 
 
 
