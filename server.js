e4e36db9f9fecb7786c5ed0a5c97492bebadf3af
Samuel Victor <samuellwat978@gmail.com>
2024-07-29 22:14:43 -0300

    Primeiro_Acerto

--- server.js
+++ server.js
@@ -18,82 +18,96 @@ const criando_tabelas_motores = async () => {
 
         await pool.query(
             `
-             CREATE TABLE IF NOT EXISTS Dados_Motor_01 (
-                 ID SERIAL PRIMARY KEY ,
-                 Motor VARCHAR(20),
-                 Temperatura NUMERIC (10, 2) NOT NULL,
-                 Frequencia NUMERIC (10, 2) NOT NULL,
-                 Corrente NUMERIC (10, 2) NOT NULL,
-                 Vibracao VARCHAR(10),
-                 PicoMax NUMERIC(10, 2) NOT NULL,
+             CREATE TABLE IF NOT EXISTS numero_coleta (
+                 ColetaID SERIAL PRIMARY KEY,
                  DataHora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
              );
              
             
-            CREATE TABLE IF NOT EXISTS Dados_Motor_02 (
-                ID SERIAL PRIMARY KEY ,
-                Motor VARCHAR(20),
+            CREATE TABLE IF NOT EXISTS dados_sensores_motores (
+                ColetaID INT REFERENCES numero_coleta(ColetaID),
+                MotorID VARCHAR(20),
                 Temperatura NUMERIC (10, 2) NOT NULL,
                 Frequencia NUMERIC (10, 2) NOT NULL,
                 Corrente NUMERIC (10, 2) NOT NULL,
                 Vibracao VARCHAR(10),
-                PicoMax NUMERIC(10, 2) NOT NULL,
-                DataHora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                PicoMax NUMERIC(10, 2) NOT NULL
             );
             ` 
         );
-         console.log('Tabelas motores criadas com sucesso');
     } catch (err) {
         console.error('Erro ao criar tabelas motores ou tabelas já existentes :', err);
     } finally {
-        //await pool.end();
-        //console.log('Conexão com Banco de Dados challenge_reply_db encerrada'); (Corrigir console log com frase adequada)
+        console.log('Tabelas motores criadas com sucesso');
     }
 };
 
 criando_tabelas_motores();
 
+
 // Rotas HTTP
 
-app.post('/inserir_dados_motor_01', async (req, res) => {
+app.post('/inserir_dados_motores', async (req, res) => {
 
-    const dados_motor_01 = req.body;
-    console.log('Dados do Motor 1 recebidos com Sucesso', dados_motor_01);
+    const dados_sensores_motores_var = req.body.dados_sensores_motores[0];
+    console.log('Dados dos motores recebidos com Sucesso', dados_sensores_motores_var);
     const client = await pool.connect();
         
     try {
         console.log('Conectando ao Banco de Dados challenge_reply_db para inserir dados do motor 01');
 
-        await client.query(
-            `
-            INSERT INTO Dados_Motor_01 (Motor, Temperatura, Frequencia, Corrente, Vibracao, PicoMax) VALUES ($1, $2, $3, $4, $5, $6)
-            ` , [dados_motor_01.Motor, dados_motor_01.Temperatura, dados_motor_01.Frequencia, dados_motor_01.Corrente, dados_motor_01.Vibracao, dados_motor_01.PicoMax]
+
+        const coletaResultado = await client.query(
+            `INSERT INTO numero_coleta (DataHora) VALUES (CURRENT_TIMESTAMP) RETURNING ColetaID;`
         );
-        console.log('Dados do Motor 01 Inseridos com Sucesso no Banco de Dados');
-        res.status(200).send('Dados do Motor 01 inseridos com sucesso.');
+        const coletaID = coletaResultado.rows[0].ColetaID;
+
+        for (const [MotorID, dados] of Object.entries(dados_sensores_motores_var)) {
+            await client.query(
+                `INSERT INTO dados_sensores_motores (ColetaID, MotorID, Temperatura, Frequencia, Corrente, Vibracao, PicoMax) 
+                VALUES ($1, $2, $3, $4, $5, $6, $7);`,
+                [coletaID, MotorID, dados.Temperatura, dados.Frequencia, dados.Corrente, dados.Vibracao, dados.PicoMax]
+            );
+        }
+        await client.query('COMMIT');
+        console.log('Dados dos Sensores dos Motores Inseridos com Sucesso no Banco de Dados challenge_reply_db');
+        res.status(200).send('Dados dos Sensores dos Motores inseridos com sucesso.');
     } catch (err) {
-        console.error('Erro ao inserir dados do motor 01.', err);
-        res.status(500).send('Erro ao inserir dados do motor 01 no banco de dados challenge_reply_db.');
+        console.error('Erro ao inserir Dados dos Sensores dos Motores no Banco de Dados challenge_reply_db', err);
+        res.status(500).send('Erro ao inserir Dados dos Sensores dos Motores no Banco de Dados challenge_reply_db.');
     } finally{
         client.release();
         console.log('Conexão com Banco de Dados challenge_reply_db liberada');
     }
 });
 
-app.get('/coletando_dados_motor_01', async (req, res) =>{
+app.get('/coletando_dados_motores/:coletaID', async (req, res) =>{
 
     let rows;
     const client = await pool.connect();
     try {
-        console.log('Conectando ao Banco de Dados challenge_reply_db para inserir dados do motor 01');
+        console.log('Conectando ao Banco de Dados challenge_reply_db para inserir dados dos sensores dos motores');
 
-        const consulta = await client.query(`SELECT * FROM Dados_Motor_01`);
-        rows = consulta.rows;
-        console.log('Dados do Motor 01 Coletados com Sucesso');
-        res.status(200).json(rows);
+        const consulta = await client.query(`
+        SELECT
+            c.ColetaID,
+            c.DataHora,
+            s.MotorID,
+            s.Temperatura,
+            s.Frequencia,
+            s.Corrente,
+            s.Vibracao,
+            s.PicoMax
+        FROM numero_coleta c
+        JOIN dados_sensores_motores s ON c.ColetaID = s.ColetaID
+        WHERE c.ColetaID = 1;`,[coletaID]
+    );
+     rows = consulta.rows;
+     console.log('Dados do Motores Coletados com Sucesso do banco de dados challenge_reply_db');
+     res.status(200).json(rows);
     } catch (err) {
-        console.error('Erro ao coletar dados do motor 01 no banco de dados challenge_reply_db');
-        res.status(500).send('Erro ao Coletar dados do Motor 01.', err);
+        console.error('Erro ao coletar dados dos Sensores dos motores no banco de dados challenge_reply_db');
+        res.status(500).send('Erro ao Coletar dados dos Sensores dos Motores do banco de dados challenge_reply_db.', err);
     } finally{
         client.release();
         console.log('Conexão com Banco de Dados challenge_reply_db liberada');
