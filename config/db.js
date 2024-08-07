3efce631f8085be2274d91b74d1b60c6e8cb31a2
Samuel Victor <samuellwat978@gmail.com>
2024-08-07 09:06:15 -0300

    First_Modulationn

--- config/db.js
+++ config/db.js
@@ -2,11 +2,9 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.user,
-    host: process.env.host,
-    database: process.env.database,
-    password: process.env.password,
-    port: process.env.port
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.ssl
+    //ssl: process.env.ssl === 'true' ? { rejectUnauthorized: false } : false
 });
 
-module.exports = pool;
+module.exports = pool
