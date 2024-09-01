dca45915007ffae31e3b19fbecfb12cf53096877
Samuel Victor <samuellwat978@gmail.com>
2024-09-01 08:52:07 -0300

    Teste_3_Sem_SSL

--- config/db.js
+++ config/db.js
@@ -4,7 +4,10 @@ require('dotenv').config();
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     //ssl: process.env.ssl
-    ssl: process.env.ssl === 'true' ? { rejectUnauthorized: false } : false
+    //ssl: process.env.ssl === 'true' ? { rejectUnauthorized: false } : false
+    ssl:{
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool
